a5d155fbd65f60777370fc4b570584c15f7fa9a9
medigor <medigor@github.com>
2023-10-02 23:40:48 +0300

    доработки

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,20 @@
-FROM docker.io/debian:bullseye-slim AS base
-WORKDIR /app
-EXPOSE 8080
+FROM docker.io/library/rust:1-bullseye AS build
+RUN apt-get -y update && \
+    apt-get -y install musl musl-dev musl-tools && \
+    rustup target add x86_64-unknown-linux-musl
+ENV RUST_BACKTRACE=1 \
+    CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
 
-FROM docker.io/rust:1-bullseye AS build
 WORKDIR /usr/src/app
 RUN USER=root cargo init
 COPY ./Cargo.toml .
 COPY ./Cargo.lock .
-RUN cargo build --release
+RUN cargo build --release --target x86_64-unknown-linux-musl
 COPY ./src ./src
-RUN touch src/main.rs && cargo build --release
+RUN touch src/main.rs && cargo build --release --target x86_64-unknown-linux-musl
 
-FROM base AS final
-WORKDIR /app
-COPY --from=build /usr/src/app/target/release/test-api .
-ENV RUST_BACKTRACE=full
-ENTRYPOINT ["/app/test-api"]
+FROM scratch
+COPY --from=build /usr/src/app/target/x86_64-unknown-linux-musl/release/test-api .
+# COPY --from=build /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
+ENV RUST_BACKTRACE=1
+ENTRYPOINT ["/test-api"]
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,68 @@
+use axum::{
+    extract::Path,
+    http::{header, HeaderMap, Method, StatusCode},
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::Serialize;
 use std::{
+    collections::HashMap,
     sync::atomic::{AtomicU64, Ordering},
     time::Duration,
 };
-
-use axum::{extract::Path, routing::post, Json, Router};
-use serde::Serialize;
+use tokio::signal;
 use tokio::time::sleep;
+use tower_http::cors::{Any, CorsLayer};
 
 #[derive(Serialize)]
 struct CounterResponse {
     counter: u64,
 }
 
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
+}
+
+async fn headers(headers: HeaderMap) -> impl IntoResponse {
+    let mut map = HashMap::<&str, &str>::new();
+    for (name, value) in headers.iter() {
+        map.insert(name.as_str(), value.to_str().unwrap_or("invalid string"));
+    }
+    let json = serde_json::to_string(&map).unwrap();
+
+    ([(header::CONTENT_TYPE, "application/json")], json)
+}
+
 #[tokio::main]
 async fn main() {
     let counter = Box::leak(Box::new(AtomicU64::new(1)));
+
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
 
     let app = Router::new()
         .route(
@@ -32,13 +80,20 @@ async fn main() {
         .route(
             "/sleep/:duration",
             post(|Path(duration): Path<u64>| async move {
+                if duration > 30_000 {
+                    return StatusCode::BAD_REQUEST;
+                }
                 sleep(Duration::from_millis(duration)).await;
-                "Ok"
+                StatusCode::OK
             }),
-        );
+        )
+        .route("/headers", get(headers))
+        .route("/headers", post(headers))
+        .layer(cors);
 
     axum::Server::bind(&([0, 0, 0, 0], 8080).into())
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
 }
